1ff557e86482b9ac11d0b51ada0fc9e978401956
Deagarys <37025496+Deagarys@users.noreply.github.com>
2025-02-12 13:18:22 +0100

    Qotd command (#2)
    * .
    
    * implement question of the day feature with command and scheduled job

---
+++ commands/qotd.js
@@ -0,0 +1,25 @@
+﻿const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits, EmbedBuilder} = require('discord.js');
+const { qotd } = require('../src/qotd');
+const { PermissionsBitField } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('qotd')
+        .setDescription('Make the bot ask a question of the day'),
+    run: async ({interaction, client, handler}) => {
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages))
+            return interaction.reply({
+                content: `You do not have permission to run this command!`,
+                flags: 64
+            });
+
+        await qotd(client);
+        console.log("🔹  - qotd command ran");
+
+        interaction.reply({
+            content: `Question of the day has been asked!`,
+            flags: 64
+        });
+    },
+    devOnly: false,
+};
---
+++ src/qotd.js
@@ -0,0 +1,49 @@
+﻿const Suggestions = require("../models/Suggestions");
+const {EmbedBuilder} = require("discord.js");
+
+module.exports = {
+    qotd: async (client) => {
+        const count = await Suggestions.countDocuments({approved: true, asked: false}, {});
+        var random = Math.floor(Math.random() * count)
+
+        const question = await Suggestions.findOne({approved: true, asked: false}).skip(random);
+
+        if (question === undefined || question === null) {
+            console.log("❌ - No qotd in db");
+            const embed = noQotdEmbed("There are currently no questions. Use /suggest to suggest more questions!");
+            await client.channels.cache.get(process.env.QOTD_CHANNEL_ID).send({
+                content: `<@&${process.env.QOTD_ROLE_ID}>`,
+                embeds: [embed]
+            });
+            return;
+        }
+
+        const embed = qotdEmbed(question.question, question.imageUrl);
+        let message = await client.channels.cache.get(process.env.QOTD_CHANNEL_ID).send({
+            content: `<@&${process.env.QOTD_ROLE_ID}>`,
+            embeds: [embed]
+        });
+
+        question.asked = true;
+        await question.save();
+
+        console.log("🔹  - qotdJob ran")
+    }
+}
+
+function qotdEmbed(question, imageUrl) {
+    return new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Question of the day!')
+        .setImage(imageUrl)
+        .setDescription(question)
+        .setTimestamp()
+}
+
+function noQotdEmbed(question) {
+    return new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setTitle('Question of the day!')
+        .setDescription(question)
+        .setTimestamp()
+}
